d1cfbf13f7b1680f1e0db3be0b81ec45c22fb592
Forwah Amstrong <lmsoftware2023@gmail.com>
2025-01-03 19:57:32 +0100

    Update setup.py
    added version specific requirements or dependencies

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ keywords='Classsroom-Insights pyqt5',
 package_dir={'ClasssroomInsights': ''},
 packages=['ClasssroomInsights'],
 py_modules =['__main__','about', 'Advanced_stats','Class_Insights_scores','Database_Manager','Glossary','Pedagogy','resources_rc','statsfuncs'],
-install_requires=['PyQt5','matplotlib','scipy','reportlab','appdirs'],
+install_requires=['PyQt5==5.15.11','matplotlib==3.10.0','scipy==1.15.0','reportlab==4.2.5','appdirs==1.4.4','Pillow==11.1.0'],
 include_package_data=True,
 entry_points={
 'console_scripts': [
